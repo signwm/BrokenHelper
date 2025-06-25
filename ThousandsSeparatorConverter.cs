6f84e4179753b33a26f45b79fd427c222665c829
signwm <social.346@outlook.com>
2025-06-25 11:29:19 +0200

    Format numbers with thousand separators

---
+++ ThousandsSeparatorConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BrokenHelper
+{
+    public class ThousandsSeparatorConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            if (value is IFormattable formattable)
+                return formattable.ToString("N0", CultureInfo.InvariantCulture).Replace(',', ' ');
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
